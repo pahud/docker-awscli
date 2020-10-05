a5a9a1ecf63657ce2cba8befd22a7e2c4337fa01
Pahud <pahudnet@gmail.com>
2020-10-05 15:56:07 +0800

    bump projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,11 @@
 const {
   AwsCdkTypeScriptApp,
+  GithubWorkflow,
   Semver
 } = require('projen');
+
+const AUTOMATION_TOKEN = 'GITHUB_TOKEN';
+
 
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: "1.63.0",
@@ -21,6 +25,50 @@ const project = new AwsCdkTypeScriptApp({
 
 project.addDependencies({
   "@pahud/aws-codebuild-patterns": Semver.caret('1.1.0'),
+});
+
+
+// create a custom projen and yarn upgrade workflow
+const workflow = new GithubWorkflow(project, 'ProjenYarnUpgrade');
+
+workflow.on({
+  schedule: [{
+    cron: '0 6 * * *'
+  }], // 6am every day
+  workflow_dispatch: {}, // allow manual triggering
+});
+
+workflow.addJobs({
+  upgrade: {
+    'runs-on': 'ubuntu-latest',
+    'steps': [
+      ...project.workflowBootstrapSteps,
+
+      // yarn upgrade
+      {
+        run: `yarn upgrade`
+      },
+
+      // upgrade projen
+      {
+        run: `yarn projen:upgrade`
+      },
+
+      // submit a PR
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v3',
+        with: {
+          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
+          'commit-message': 'chore: upgrade projen',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade projen and yarn',
+          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+          'labels': 'auto-merge',
+        }
+      },
+    ],
+  },
 });
 
 
